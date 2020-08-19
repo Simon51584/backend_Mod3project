ba0938fca04d806b4541a490c548fd64ed58d87c
Simon51584 <simon51584@gmail.com>
2020-08-19 17:45:03 -0500

    Backend update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,33 @@
 class GamesController < ApplicationController
+    def index
+        @game = Game.all
+        options = {
+            include:[:player]
+        }
+        render json: @game.to_json(
+            include:{
+                player:{
+                    only:%i[id name]
+                }
+            }
+        )
+    end
+    
+    def show
+        game = Game.find(params[:id])
+        render json: game
+    end
+
+    def create
+        
+    end
+
+    def update
+        
+    end
+
+    def destroy
+    
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :players
+  resources :games, only:[:index,:show,:update,:create,:delete]
+  resources :players, only:[:show,:create,:update,:delete]
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
