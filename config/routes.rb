39b8f627ba62a16df2b5917bfbd2a39a2e0d7e22
Simon51584 <simon51584@gmail.com>
2020-09-23 11:29:52 -0500

    updated players route index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :games, only:[:index,:show,:update,:create,:destroy]
-  resources :players, only:[index, :show,:create,:update,:destroy]
+  resources :players, only:[:index, :show,:create,:update,:destroy]
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
