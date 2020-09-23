fe7b85b319c941b3a708fe8efdfcdd47f82e4b6c
Simon51584 <simon51584@gmail.com>
2020-09-22 23:57:49 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2020_08_17_193540) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
-    t.integer "player_id"
+    t.bigint "player_id"
     t.integer "score"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
